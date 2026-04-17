1f7044118ffd3a03510ce8e9ffec5a7bbe5b487a
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:59:32 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Map 3royxatdagi Har Bir Sozga Uning Uzunligini Qoshib Yangi 
+Topic: MAP – 3:��Ro‘yxatdagi har bir so‘zga uning uzunligini qo‘shib, yangi formatda qaytarish: royxat = ["Ali", "Bobur", "Stive", "Santyago"]
+"""
+
+def main():
+    print("Welcome to Map 3royxatdagi Har Bir Sozga Uning Uzunligini Qoshib Yangi !")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
